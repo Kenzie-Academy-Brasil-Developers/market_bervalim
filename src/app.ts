52f58955d44bf5e9986399c027223a48c7fd89f2
bervalim <bernardogvalim@gmail.com>
2023-09-21 17:56:50 -0300

    feat/creating-endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,23 @@
-import express, { Request, Response } from "express";
+import express from "express";
 
 const app = express();
 
 app.use(express.json());
+
+// Criar e adicionar o produto
+app.post("/products");
+
+// Listar todos os produtos:
+app.get("/products");
+
+// Listar um produto específico a partir de seu id:
+app.get("/products/:id");
+
+// Atualizar os dados a partir do id
+app.patch("/products/:id");
+
+// Deletar o produto a partir do id:
+app.delete("/products/:id");
 
 const PORT = 3000;
 
